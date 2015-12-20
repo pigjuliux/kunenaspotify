fac2aeae174c553d7db55f21aa011b21bba95b5c
pigjuliux <juliux.pigface@gmail.com>
2015-12-20 13:27:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+kunenaspotify
+===============
+Spotify Plugin for the Kunena Forum.
+
+License
+------------
+See "./development/LICENSE.txt"
+If you download and use this add-on, please be sure to read and understand the license, especially the part related to the Spotify Widget.
+Please note that this plugin is Open Source software, but the Spotify Widget itself, is not.
+
+Further information are available here:
+https://developer.spotify.com/technologies/widgets/terms-of-use/
+
+Installation
+------------
+Download the latest ".zip" from the folder "releases" and install it with the appropriate Joomla interface.
+There also is a little how-to, located into the "docs" subfolder of the "development" path.
+
+Requirements
+------------
+kunenaspotify is a Joomla plugin, so it's not bound to a specific release.
+See "./development/REQUIREMENTS.txt" for a list of tested versions of Kunena, Joomla, MariaDB, Php and Apache Httpd.
+
+Development and issues
+------------
+kunenaspotify is Open Source, so I'll be glad to review improvements and suggestions for the codebase.
+Feel free to open an issue here if you discover a bug or experience problems.
---
+++ development/kunenaspotify.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+ * Kunena Spotify Plugin
+ * Author  : Giulio 'juliux' Erler
+ * Contact : juliux {DOT} pigface {AT} gmail {DOT} com
+ * Web : http://pigjuliux.wordpress.com
+ * License of this plugin : GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.plugin.plugin');
+
+class plgKunenaKunenaSpotify extends JPlugin
+{
+
+    public function __construct(&$subject, $config)
+    {
+        parent::__construct($subject, $config);
+    }
+
+    public function onKunenaBbcodeEditorInit($editor)
+    {
+        $this->loadLanguage();
+        $btn = new KunenaBbCodeEditorButton('spotify', 'spotify', 'spotify', 'PLG_KUNENASPOTIFY_BTN_TITLE', 'PLG_KUNENASPOTIFY_BTN_ALT');
+        $btn->addWrapSelectionAction();
+        $editor->insertElement($btn, 'after', 'code');
+
+        $url = $this->params->get('mathjax', 'https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML');
+        $document = JFactory::getDocument();
+        $document->addScript($url);
+
+        $document->addStyleDeclaration("#Kunena #kbbcode-toolbar #spotify {
+            background-image: url(\"" . JURI::base(true) . "/plugins/kunena/kunenaspotify/images/spotify.png\");
+        }");
+
+		//Add JS Code for preview
+		$document->addScriptDeclaration("window.addEvent('domready', function() {
+			preview = document.id('kbbcode-preview');
+			preview.addEvent('updated', function(event){
+				MathJax.Hub.Queue(['Typeset',MathJax.Hub,'kbbcode-preview']);
+				document.getElements('.latex').each(function(item, index) {
+                    item.setStyle('display', '');
+                });
+			});
+		});");
+
+    }
+
+    public function onKunenaBbcodeConstruct($bbcode)
+    {
+        $bbcode->AddRule('spotify', array(
+                'mode' => BBCODE_MODE_CALLBACK,
+                'method' => 'plgKunenaKunenaSpotify::onSpotify',
+                'allow' => array('type' => '/^[\w]*$/',),
+                'allow_in' => array('listitem', 'block', 'columns'),
+                'content' => BBCODE_VERBATIM,
+                'before_tag' => "sns",
+                'after_tag' => "sn",
+                'before_endtag' => "sn",
+                'after_endtag' => "sns",
+                'plain_start' => "\n",
+                'plain_end' => "\n")
+        );
+
+        $url = $this->params->get('mathjax', 'https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML');
+
+        $document = JFactory::getDocument();
+        $document->addScript($url);
+        $document->addScriptDeclaration("window.addEvent('domready', function() {
+            document.getElements('.latex').each(function(item, index) {
+                item.setStyle('display', '');
+            });
+        });");
+
+        return true;
+    }
+
+    static public function onSpotify($bbcode, $action, $name, $default, $params, $content)
+    {
+
+        if ($action == BBCODE_CHECK) {
+            $bbcode->autolink_disable = 1;
+            return true;
+        }
+
+        $bbcode->autolink_disable = 0;
+        $config = JPluginHelper::getPlugin('kunena', 'kunenaspotify');
+        $config = json_decode($config->params);
+        $html = '<iframe src="https://embed.spotify.com/?uri=spotify:track:'.$content.'" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>';
+        
+        return $html;
+    }
+}
+
